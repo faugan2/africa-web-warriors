5806c3ae6ff0088efcecb1bb815f18c8d15556c8
faugan <foganbidi2@gmail.com>
2022-04-01 22:49:05 +0100

    changes

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -8,6 +8,14 @@ import EditIcon from '@material-ui/icons/Edit';
 import CameraAltIcon from '@material-ui/icons/CameraAlt';
 import Steps from "../components/Steps";
 
+import helpdesk from "../components/img/helpdesk.png";
+import security from "../components/img/security.jpg";
+import networking from "../components/img/networking.png";
+import cloud from "../components/img/cloud.png";
+import ethical_hacking from "../components/img/ethical_hacking.jpeg";
+import programmation from "../components/img/programmation.jpg";
+import linux from "../components/img/linux.png";
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
 const Profile=()=>{
     const user=useSelector(selectUser);
 
@@ -32,7 +40,46 @@ const Profile=()=>{
                             <button><EditIcon /></button>
                         </div>
 
-                        <Steps />
+                        {/*<Steps />*/}
+
+                        
+                    </div>
+                    <div className="matieres">
+                        <div className="line progress">
+                            <img src={programmation} />
+                            <p>Programmation</p>
+                            <CheckCircleIcon className="status progress"/>
+                        </div>
+                        <div className="line progress">
+                            <img src={linux} />
+                            <p>Système</p>
+                            <CheckCircleIcon className="status progress"/>
+                        </div>
+                        <div className="line progress">
+                            <img src={helpdesk} />
+                            <p>Help-Desk</p>
+                            <CheckCircleIcon className="status progress"/>
+                        </div>
+                        <div className="line progress">
+                            <img src={security} />
+                            <p>Security</p>
+                            <CheckCircleIcon className="status progress"/>
+                        </div>
+                        <div className="line progress">
+                            <img src={networking} />
+                            <p>Networking</p>
+                            <CheckCircleIcon className="status progress"/>
+                        </div>
+                        <div className="line progress">
+                            <img src={cloud} />
+                            <p>Cloud</p>
+                            <CheckCircleIcon className="status progress"/>
+                        </div>
+                        <div className="line progress">
+                            <img src={ethical_hacking} />
+                            <p>Ethical Hacking</p>
+                            <CheckCircleIcon className="status progress"/>
+                        </div>
                     </div>
                 </div>
                 <Footer />
