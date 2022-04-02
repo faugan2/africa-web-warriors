fc6448274479d43ff1ba38e1e089fae10d8e5bf3
faugan <foganbidi2@gmail.com>
2022-04-02 20:59:59 +0100

    changes

--- src/components/LessonNote.js
+++ src/components/LessonNote.js
@@ -1,5 +1,7 @@
 import "../styles/lesson_note.scss";
 import no_data from "./img/no_data.jpg";
+import FolderIcon from '@material-ui/icons/Folder';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
 
 const LessonNote=()=>{
     return(
@@ -7,6 +9,15 @@ const LessonNote=()=>{
             <div className="no_data">
                 <img src={no_data} />
                 <p>Aucune note n'est trouvée</p>
+            </div>
+
+            <div className="floating_btn">
+                <button>
+                    <FolderIcon />
+                </button>
+                <button>
+                    <WhatsAppIcon />
+                </button>
             </div>
         </div>
     )
