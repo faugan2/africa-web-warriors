b70450fdc6a4418c9800c1536ed883cb1e175851
faugan <foganbidi2@gmail.com>
2022-04-02 22:09:26 +0100

    changes

--- src/components/LessonTask.js
+++ src/components/LessonTask.js
@@ -1,8 +1,13 @@
 import "../styles/lesson_task.scss";
+import no_data from "./img/no_data.jpg";
+
 const LessonTask=()=>{
     return(
         <div className="lesson_task">
-            exercice
+            <div className="no_data">
+                <img src={no_data} />
+                <p>Aucun exercie n'est lié à ce chapitre</p>
+            </div>
         </div>
     )
 }
