082c877644756ebb63c68a2a65e6b94e01680bd4
faugan <foganbidi2@gmail.com>
2022-04-02 16:45:07 +0100

    changes

--- src/components/LessonContent.js
+++ src/components/LessonContent.js
@@ -1,8 +1,12 @@
+import ReactPlayer from 'react-player/lazy'
 import "../styles/lesson_content.scss";
+import media from "./video/2.mp4";
+
 const LessonContent=()=>{
     return(
         <div className="chapter_content">
-            contenu du chapitre
+
+             <video src={media} controls ></video>
         </div>
     )
 }
