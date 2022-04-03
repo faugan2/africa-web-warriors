af8b68b6191a2190c0602179758006186290b885
faugan <foganbidi2@gmail.com>
2022-04-03 15:17:32 +0100

    changes

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -51,7 +51,7 @@ const HomeContent=()=>{
                    </div>
                </div>
                <div className="right">
-                <ReactPlayer url='https://www.youtube.com/watch?v=nbmJnCHXaW8' width={500} height={300} />
+                <ReactPlayer url='https://www.youtube.com/watch?v=gbgOVA2l78o' width={500} height={300} />
                </div>
            </div>
 
