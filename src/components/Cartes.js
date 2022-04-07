ffbad4fcbe37a6f49822913c2210f621a8aad0bc
faugan <foganbidi2@gmail.com>
2022-04-07 14:49:31 +0100

    changes

--- src/components/Cartes.js
+++ src/components/Cartes.js
@@ -3,7 +3,7 @@ import "../styles/cartes_mobile.scss";
 import appli_web from "./img/appli_web.png";
 import appli_mobile from "./img/appli_mobile.png";
 import appli_desktop from "./img/appli_desktop.png";
-
+import ethical_hacking from "./img/ethical_hacking.jpeg";
 const Cartes=()=>{
     return(
         <div className="cartes">
@@ -15,7 +15,9 @@ const Cartes=()=>{
                                     <h2>Création des applications web</h2>
                                     <p>
                                     Vous souhaitez maîtriser la programmation web et devenir un as du langage informatique ? 
-                                    Obtenez votre qualification d'intégrateur web pour travailler en freelance, en agence ou en entreprise. 
+                                    <br /><br />
+                                    Obtenez votre qualification d'intégrateur web pour travailler en freelance, 
+                                    en agence ou en entreprise. <br /><br />
                                     Réalisez votre formation à distance, à votre rythme. 
                                     Vous serez accompagné par nos formateurs experts
                                     </p>
@@ -35,7 +37,14 @@ const Cartes=()=>{
                                 <div>
                                     <h2>Création des applications mobile</h2>
                                     <p>
-                                    Avec l’essor des nouvelles technologies et la multiplication des smartphones, des techniciens du digital naissent. Le métier de développeur d’applications mobiles participe au boom numérique : désormais les entreprises ont besoin d’artisans du digital. Le développeur a pour coeur de métier de créer des applications, d’adapter des sites web sur mobile ou bien d’en optimiser des existantes. Il va développer et concevoir.
+                                    Avec l’essor des nouvelles technologies et la multiplication des smartphones, 
+                                    des techniciens du digital naissent.
+                                    <br /><br />
+                                     Le métier de développeur d’applications mobiles
+                                     participe au boom numérique : désormais les entreprises ont besoin d’artisans du digital. 
+                                     <br /><br />
+                                     Le développeur a pour coeur de métier de créer des applications, d’adapter des sites web sur
+                                      mobile ou bien d’en optimiser des existantes. Il va développer et concevoir.
                                     </p>
                                     <p className="tech">
                                         <span>Technologies: </span>
@@ -56,12 +65,12 @@ const Cartes=()=>{
                                     <p>
                                     Le développement d’applications desktop t’intéresse et tu souhaites en 
                                     apprendre davantage ? Pour ta culture personnelle, par curiosité ou bien 
-                                    tout simplement pour des raisons professionnelles ?
+                                    tout simplement pour des raisons professionnelles ?<br /><br />
                                     En tant que développeur, tu souhaites créer une petite application mais tu ne veux pas 
-                                    apprendre un langage lourd et souhaites réutiliser tes connaissances HTML/CSS/JS ?
+                                    apprendre un langage lourd et souhaites réutiliser tes connaissances HTML/CSS/JS ?<br /><br />
 
                                     Tu as entendu parler d’un outil permettant de simplement créer une appli cross-plateforme 
-                                    facilement nommée Electron ?
+                                    facilement nommée Electron ?<br/>
                                     Et bien bonne nouvelle, tu as trouvé LA formation qu’il te faut !
                                     </p>
                                     <p className="tech">
@@ -77,6 +86,38 @@ const Cartes=()=>{
                                 </div>
                                
                             </div>
+
+
+                            <div>
+                                <img src={ethical_hacking} />
+                                <div>
+                                    <h2>cybersécurité (Hacking Ethique)</h2>
+                                    <p>
+                                    Bienvenue dans ce cours vidéo sur le hacking éthique !<br /><br />
+
+À travers celui-ci, vous allez apprendre beaucoup de concepts fondamentaux en sécurité informatique.<br /><br />
+
+Le cours se veut à la fois pratique et interactif, vous aurez de quoi vous entraîner sur vos systèmes sans rien casser et vous pourrez poser vos questions à tout moment si vous en avez.
+<br /><br />
+Les cyberattaques les plus populaires seront traitées dans ce cours, de façon à vous enseigner les bases de la cybersécurité dans les grands domaines informatiques (web, réseau, système, etc). 
+Le tout dans le respect des lois et de l'éthique du hacking.
+                                  </p>
+                                    <p className="tech">
+                                        <span>Technologies: </span>
+                                        Programmation, Systèm, Securité, Réseau, Cloud, Pentest
+                                        </p>
+                                    <p className="delai">
+                                        <span>Delai: </span>
+                                        9 mois</p>
+                                    <p className="prix">
+                                        <span>Coût: </span>
+                                        150 000CFA</p>
+                                    <button className="btn_start">Commencez la formation</button>
+                                </div>
+                               
+                            </div>
+
+
                         </div>
     );
 }
