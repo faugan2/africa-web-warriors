1b7d335e90e9b99afd654c83ff350c96f923ca3a
faugan <foganbidi2@gmail.com>
2022-04-07 17:37:29 +0100

    changes

--- src/components/FormationsList.js
+++ src/components/FormationsList.js
@@ -3,10 +3,23 @@ import "../styles/formations_list_mobile.scss";
 import {data} from "./formations";
 import {useState,useEffect} from "react";
 import {useSelector,useDispatch} from "react-redux";
-import { setAjouter } from "../features/appSlice";
+import { setAjouter,selectAchats,selectUser } from "../features/appSlice";
 
 const FormationsList=({click,open_code})=>{
     const dispatch=useDispatch();
+
+    const me=useSelector(selectUser);
+    const achats=useSelector(selectAchats);
+
+    const [mes_achats,set_mes_achats]=useState(null);
+
+    useEffect(()=>{
+        const res=achats.filter((item)=>{
+            return item.user==me?.key;
+        })
+        set_mes_achats(res);
+
+    },[achats]);
 
     const add_formation=(id)=>{
         dispatch(setAjouter(id)); 
@@ -17,6 +30,12 @@ const FormationsList=({click,open_code})=>{
         <div className="formations_list">
             {
                 data?.map((item,i)=>{
+                    
+                    const res=mes_achats?.filter((item2)=>{
+                        return item2.formation==item.id;
+                    })
+                    if(res?.length>0) return null;
+
                     return(
                         <div key={item.id} className="line">
                             <h2>{item.titre}</h2>
