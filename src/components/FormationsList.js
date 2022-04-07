38e5f68280cb167676ffe96decd960a40003a994
faugan <foganbidi2@gmail.com>
2022-04-07 16:13:54 +0100

    changes

--- src/components/FormationsList.js
+++ src/components/FormationsList.js
@@ -1,10 +1,35 @@
 import "../styles/formations_list.scss";
 import "../styles/formations_list_mobile.scss";
+import {data} from "./formations";
 
 const FormationsList=()=>{
     return(
         <div className="formations_list">
-            liste de nos formations 
+            {
+                data?.map((item,i)=>{
+                    return(
+                        <div key={i} className="line">
+                            <h2>{item.titre}</h2>
+                            <div dangerouslySetInnerHTML={{__html:item.description}} />
+                            <p className="tech">
+                                <span>Techs: </span>{item.tech}</p>
+                            <p className="delai">
+                                <span>Delai: </span>
+                                {item.delai}</p>
+                            <p className="prix">
+                                <span>Coût: </span>
+                                {item.cout}
+                            </p>
+                            {item.bonus!=undefined && <p className="bonus">
+                                <span>Bonus: </span>
+                                {item.bonus}
+                                </p>}
+                            <button>Ajouter</button>
+                            
+                        </div>
+                    )
+                })
+            }
         </div>
     );
 }
---
+++ src/components/formations.js
@@ -0,0 +1,68 @@
+const data=[
+    {
+        id:"1",
+        titre:"Création des applications web",
+        description:`Vous souhaitez maîtriser la programmation web et devenir un as du langage informatique ? 
+        <br /><br />
+        Obtenez votre qualification d'intégrateur web pour travailler en freelance, 
+        en agence ou en entreprise. <br /><br />
+        Réalisez votre formation à distance, à votre rythme. 
+        Vous serez accompagné par nos formateurs experts`,
+        tech:"HTML, CSS, Javascript, Firebase, ReactJS",
+        delai:"2 mois",
+        cout:"50 000 FCFA",
+        bonus:"2 livres offerts",
+    },
+    {
+        id:"2",
+        titre:"Création des applications mobile",
+        description:`Avec l’essor des nouvelles technologies et la multiplication des smartphones, 
+        des techniciens du digital naissent.
+        <br /><br />
+         Le métier de développeur d’applications mobiles
+         participe au boom numérique : désormais les entreprises ont besoin d’artisans du digital. 
+         <br /><br />
+         Le développeur a pour coeur de métier de créer des applications, d’adapter des sites web sur
+          mobile ou bien d’en optimiser des existantes. Il va développer et concevoir.
+        `,
+        tech:"React Native",
+        delai:"1 mois",
+        cout:"30 000 FCFA",
+        bonus:"",
+    },
+    {
+        id:"3",
+        titre:"Création des applictions desktop",
+        description:`Le développement d’applications desktop t’intéresse et tu souhaites en 
+        apprendre davantage ? Pour ta culture personnelle, par curiosité ou bien 
+        tout simplement pour des raisons professionnelles ?<br /><br />
+        En tant que développeur, tu souhaites créer une petite application mais tu ne veux pas 
+        apprendre un langage lourd et souhaites réutiliser tes connaissances HTML/CSS/JS ?<br /><br />
+
+        Tu as entendu parler d’un outil permettant de simplement créer une appli cross-plateforme 
+        facilement nommée Electron ?<br/>
+        Et bien bonne nouvelle, tu as trouvé LA formation qu’il te faut !`,
+        tech:"Electron JS",
+        delai:"1 mois",
+        cout:"30 000 FCFA",
+        bonus:"",
+    },
+    {
+        id:"4",
+        titre:"cybersécurité (Hacking Ethique)",
+        description:`Bienvenue dans ce cours vidéo sur le hacking éthique !<br /><br />
+
+        À travers celui-ci, vous allez apprendre beaucoup de concepts fondamentaux en sécurité informatique.<br /><br />
+        
+        Le cours se veut à la fois pratique et interactif, vous aurez de quoi vous entraîner sur vos systèmes sans rien casser et vous pourrez poser vos questions à tout moment si vous en avez.
+        <br /><br />
+        Les cyberattaques les plus populaires seront traitées dans ce cours, de façon à vous enseigner les bases de la cybersécurité dans les grands domaines informatiques (web, réseau, système, etc). 
+        Le tout dans le respect des lois et de l'éthique du hacking.`,
+        tech:"Programmation, Systèm, Securité, Réseau, Cloud, Pentest",
+        delai:"9 mois",
+        cout:"150 000 FCFA",
+        bonus:"",
+    }
+]
+
+export {data};
