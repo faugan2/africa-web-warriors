4cb1243c9d5792558077b578631cfab7ebf045ad
faugan <foganbidi2@gmail.com>
2022-04-07 18:25:15 +0100

    changes

--- src/components/Cartes.js
+++ src/components/Cartes.js
@@ -1,8 +1,46 @@
 import "../styles/cartes.scss";
 import "../styles/cartes_mobile.scss";
 import {data} from "./formations";
+import {useSelector,useDispatch} from "react-redux";
+import {selectUser,selectAchats,setAjouter} from "../features/appSlice";
+import {useState,useEffect} from "react";
+import Modal from "./Modal";
+import Unlock from "./Unlock";
+import {useHistory} from "react-router-dom";
 
 const Cartes=()=>{
+    const me=useSelector(selectUser);
+    const achats=useSelector(selectAchats);
+
+    const dispatch=useDispatch();
+    const history=useHistory();
+
+    const [mes_achats,set_mes_achats]=useState(null);
+    const [open,set_open]=useState(false);
+
+    const close_modal=()=>{
+        set_open(false);
+    }
+
+    useEffect(()=>{
+        const res=achats?.filter((item,i)=>{
+            return item.user==me?.key
+        });
+        set_mes_achats(achats);
+    },[achats])
+
+    const detail_formation=(b,item)=>{
+        const id=item.id;
+        dispatch(setAjouter(id));
+        if(b==""){
+            set_open(true);
+            return;
+        }
+        history.push("/lesson");
+
+
+        
+    }
     return(
         <div className="cartes">
 
@@ -10,6 +48,17 @@ const Cartes=()=>{
 
             {
                 data?.map((item,i)=>{
+                    let b="";
+                    const res=mes_achats?.filter((item2)=>{
+                        return item2.formation==item.id;
+                    })
+                    if(res!=undefined){
+                        if(res.length>0){
+                            b="buy"
+                         }
+                    }
+                  
+                   //console.log("the result is ",mes_achats);
                     return(
                         <div>
                                 <img src={item.photo} />
@@ -23,14 +72,22 @@ const Cartes=()=>{
                                     <p className="prix"><span>Coût:</span> {item.cout}</p>
                                     {item.bonus !="" &&
                                     <p className="bonus"><span>Bonus: </span> {item.bonus}</p>}
-                                    <button className="btn_start">Commencez la formation</button>
+                                    <button 
+                                        className={`btn_start ${b}`} 
+                                        onClick={detail_formation.bind(this,b,item)}
+                                    >
+                                    Commencez la formation
+                                    </button>
                                 </div>
-                                
                             </div>
                     )
                 })
-            }                    
+        
+        }
+
+        {open==true && <Modal click={close_modal} content={<Unlock click={close_modal}/>} />}                    
         </div>
+
     );
 }
 
